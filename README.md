190c8026377fb7b02ec5dbba6eb4a8b23303a59e
Bhargava Charan <bhargavacharan1316@gmail.com>
2021-03-09 03:03:08 +0000

    lingress function

--- README.md
+++ README.md
@@ -26,3 +26,4 @@ Copy your project's URL and submit it to freeCodeCamp.
 ### Data Source
 Global Average Absolute Sea Level Change, 1880-2014 from the US Environmental Protection Agency using data from CSIRO, 2015; NOAA, 2015.
 https://datahub.io/core/sea-level-rise
+= 
--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -12,13 +12,33 @@ def draw_plot():
     plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
 
     # Create first line of best fit
+    x = df['Year']
+    y = df['CSIRO Adjusted Sea Level']
+    
+    slope, intercept, r_value, p_value, std_err = linregress(x,y)
+    
+    plt.plot(x, intercept + slope*x, 'r', label = 'Fitted Line 1')
+    plt.legend()
+    plt.show()
+    
+    (slope,
+     intercept,
+     r_value,
+     p_value,
+     std_err) = linregress(df[df['Year'] >= 2000]['Year'],
+                           df[df['Year'] >= 2000]['CSIRO Adjusted Sea Level'])
+    x_fit_from_2000 = pd.Series([*range(2000, 2050)])
+    y_fit_from_2000 = intercept + slope*x_fit_from_2000
+    plt.plot(x_fit_from_2000, y_fit_from_2000, 'r')
 
-
+    
     # Create second line of best fit
+    
 
 
     # Add labels and title
-
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
